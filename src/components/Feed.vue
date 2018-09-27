3d19fb7b46ed85e7bf1634b2c3406741e7e05b91
EliasManj <miguel.elias.g.manjarrez@gmail.com>
2018-09-27 12:49:13 -0500

    new post component

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,13 +1,38 @@
 <template>
     <div id="feed">
         This is the feed component
+        <br>
+        <ul>
+            <li v-for="post in posts" :key="post">
+                {{ post }}
+            </li>
+        </ul>
     </div>    
 </template>
 
 
 <script>
 export default {
-    
+    data(){
+        return {
+            posts: [
+                {
+                    title: "post 1",
+                    desc: "soe decription",
+                    username: "poop22",
+                    img: null,
+                    likes: 0
+                },
+                {
+                    title: "post 2",
+                    desc: "heyyy boiiii",
+                    username: "poop22",
+                    img: null,
+                    likes: 9999
+                }
+            ]
+        }
+    }
 }
 </script>
 
